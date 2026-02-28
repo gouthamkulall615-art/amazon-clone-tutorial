35103956a16645e30c551f483c906e004703f981
Goutham Kulall <gouthamkulall615@gmail.com>
2026-02-28 20:58:57 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'))
+
+
+if(!cart){
+    cart=[
 {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2,
@@ -7,6 +11,12 @@ export let cart = [
     quantity:1
 }
 ];
+}
+
+function saveToStorage()
+{
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
  export function addToCart(productId) {
   let matchingItem;
   cart.forEach((cartItem) => {
@@ -24,14 +34,16 @@ export let cart = [
       quantity: 1
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
 const newCart=[];
 cart.forEach((cartItem)=>{
-if(cartItem.id!==productId){
+if(cartItem.productId!==productId){
     newCart.push(cartItem)
 }
 })
 cart=newCart;
+saveToStorage();
 }
